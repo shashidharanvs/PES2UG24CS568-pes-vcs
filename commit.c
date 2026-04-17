a7964346c74f415e68502628ad9b8696acdfcd84
shashidharanvs <shashidharanvs1@gmail.com>
2026-04-17 22:16:03 +0530

    phase4: added metadata handling

--- commit.c
+++ commit.c
@@ -209,5 +209,9 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         commit.has_parent = 0;
     }
 
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = (uint64_t)time(NULL);
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+
     return -1;
 }
