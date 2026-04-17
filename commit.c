34f9275a06bd744b77ab823990029d6b2b6073f2
shashidharanvs <shashidharanvs1@gmail.com>
2026-04-17 22:15:02 +0530

    phase4: added commit_create

--- commit.c
+++ commit.c
@@ -194,8 +194,11 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
+    if (!message || !commit_id_out) return -1;
+
+    ObjectID tree_id;
+    if (tree_from_index(&tree_id) != 0) return -1;
+
+    (void)tree_id;
     return -1;
 }
